ba7ef5691285c9bf30fee731ccd406720416ecd3
no-name3 <hajatyanvahe@gmail.com>
2024-02-22 15:32:03 +0400

    Form Auto-Save to LocalStorage task

---
+++ Form Auto-Save to LocalStorage/main.js
@@ -0,0 +1,21 @@
+const btn = document.getElementById("btn");
+const impName = document.getElementById("name");
+const impEmail = document.getElementById("email");
+
+const block = document.getElementById("block");
+const p_block = document.getElementById("p_block");
+
+const p1 = document.getElementById("p1");
+const p2 = document.getElementById("p2");
+
+btn.addEventListener("click", () => {
+  localStorage.setItem("name", impName.value);
+  localStorage.setItem("email", impEmail.value);
+
+  block.style.display = "none";
+  btn.style.display = "none";
+  p_block.style.display = "flex";
+
+  p1.innerHTML = "name = " + localStorage.getItem("name");
+  p2.innerHTML = "email = " + localStorage.getItem("email");
+});
